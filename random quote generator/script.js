9bfe8306b4b9d97fc38176032e7a22b52bfe38f2
Ahmad Raza <razaahmadzafar.dev@gmail.com>
2025-10-09 12:50:21 +0500

    commit

--- random quote generator/script.js
+++ random quote generator/script.js
@@ -1,8 +1,17 @@
-const api_Key = "https://api.quotable.io"
+const api_Key = "https://random-quotes-freeapi.vercel.app/api/random"
+const quote = document.querySelector("blockquote");
+const author = document.querySelector(".author");
+const newBtn = document.querySelector("#new-quote")
 
 async function getQuote(url) {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data)
+    // console.log(data)
+    quote.innerHTML = data["quote"]
+    author.innerHTML = data["author"]
 }
 getQuote(api_Key);
+
+newBtn.addEventListener("click", function(){
+    getQuote(api_Key);
+});
